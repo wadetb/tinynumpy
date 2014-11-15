bea750b1387a0355cbae7e3a6e7923de2f308c81
Wade Brainerd <wadetb@gmail.com>
2014-11-14 23:13:13 -0500

    Implemented a selection of new functions, as yet untested, most with only basic functionality:
    all
    any
    argmax
    argmin
    clip
    cumprod
    cumsum
    mean
    prod
    ptp
    ravel
    repeat
    reshape
    arange
    eye
    
    __setitem__ now functions on ranges, in theory, but does not attempt broadcasting.
    __len__ is implemented for arrays, untested.

--- tinyndarray.py
+++ tinyndarray.py
@@ -48,7 +48,7 @@ def _assign_from_object(array, obj):
 				_assign_from_object_r(e)
 				key.pop()
 		except TypeError:
-			array[key] = element
+			array[tuple(key)] = element
 
 	_assign_from_object_r(obj)
 
@@ -127,6 +127,9 @@ class ndarray:
 		else:
 			self.data = dataarray.array(typecode, [0] * self.size)
 
+	def __len__(self):
+		return self.size
+
 	def __getitem__(self, key):
 		# Indexing spec is located at:
 		# http://docs.scipy.org/doc/numpy/reference/arrays.indexing.html
@@ -168,8 +171,13 @@ class ndarray:
 		return ndarray(tuple(shape), offset, tuple(strides), self.typecode, self)
 
 	def __setitem__(self, key, value):
-		offset = _offset_for_key(key, self.strides)
-		self.data[self.offset + offset] = value
+		view = self[key]
+		mutable_key = [0] * len(view.shape)
+		while True:
+			offset = view.offset + _offset_for_key(mutable_key, view.strides)
+			view.data[offset] = float(value)
+			if not _increment_mutable_key(mutable_key, view.shape):
+				break
 
 	def __float__(self):
 		return float(self.data[self.offset])
@@ -196,6 +204,18 @@ class ndarray:
 
 		return s
 
+	def all(self, axis=None):
+		return all(self, axis)
+
+	def any(self, axis=None):
+		return any(self, axis)
+
+	def argmax(self, axis=None):
+		return argmax(self, axis)
+
+	def argmin(self, axis=None):
+		return argmin(self, axis)
+
 	def copy(self):
 		a = empty(self.shape)
 		self_iter = nditer(self)
@@ -204,23 +224,137 @@ class ndarray:
 			a_iter[i] = self_iter[i]
 		return a
 
+	def clip(self, a_min, a_max, out=None):
+		return clip(self, a_min, a_max, out)
+
+	def cumprod(self, axis=None, out=None):
+		return cumprod(self, axis, out)
+
+	def cumsum(self, axis=None, out=None):
+		return cumsum(self, axis, out)
+
+	def fill(self, value):
+		fill(self, value)
+
 	def flatten(self):
 		a = empty((self.size,))
 		for i_index, i in enumerate(nditer(self)):
 			a[i_index,] = float(i)
 		return a
 
-	def fill(self, value):
-		fill(self, value)
-
 	def max(self, axis=None):
 		return max(self, axis)
+
+	def mean(self, axis=None):
+		return mean(self, axis)
 
 	def min(self, axis=None):
 		return min(self, axis)
 
+	def prod(self, axis=None):
+		return prod(self, axis)
+
+	def ptp(self, axis=None):
+		return ptp(self, axis)
+
+	def ravel(self):
+		return ravel(self)
+
+	def repeat(self, repeats, axis=None):
+		return repeat(self, repeats, axis)
+
+	def reshape(self, newshape):
+		return reshape(self, newshape)
+
 	def sum(self, axis=None):
 		return sum(self, axis)
+
+def all(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		for i in nditer(a):
+			if float(i) == 0:
+				return False
+		return True
+
+def any(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		for i in nditer(a):
+			if float(i) != 0:
+				return False
+		return True
+
+def argmax(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		r = float(nditer(a)[0])
+		r_index = 0
+		for i_index, i in enumerate(nditer(a)):
+			v = float(i)
+			if v > r:
+				r = v
+				r_index = i_index
+		return r_index
+
+def argmin(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		r = float(nditer(a)[0])
+		r_index = 0
+		for i_index, i in enumerate(nditer(a)):
+			v = float(i)
+			if v < r:
+				r = v
+				r_index = i_index
+		return r_index
+
+def clip(a, a_min, a_max, out):
+	if out == None:
+		out = empty(self.shape)
+	a_iter = nditer(a)
+	out_iter = nditer(out)
+	for i in xrange(self.size):
+		v = a_iter[i]
+		if v > a_max:
+			out_iter[i] = a_max
+		elif v < a_min:
+			out_iter[i] = a_min
+		else:
+			out_iter[i] = v
+	return out
+
+def cumprod(a, axis=None, out=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		if out == None:
+			out = empty((self.size,))
+		p = 1.0
+		a_iter = nditer(a)
+		out_iter = nditer(out)
+		for i in xrange(self.size):
+			p *= a_iter[i]
+			out_iter[i] = p
+		return out
+
+def cumsum(a, axis=None, out=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		if out == None:
+			out = empty((self.size,))
+		s = 0.0
+		a_iter = nditer(a)
+		out_iter = nditer(out)
+		for i in xrange(self.size):
+			s += a_iter[i]
+			out_iter[i] = s
+		return out
 
 def fill(a, value):
 	for i in nditer(a):
@@ -237,6 +371,15 @@ def max(a, axis=None):
 				r = v
 		return r
 
+def mean(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		s = 0.0
+		for i in nditer(a):
+			s += float(i)
+		return s / float(a.size)
+
 def min(a, axis=None):
 	if axis:
 		raise (TypeError, "axis argument is not supported")
@@ -247,6 +390,54 @@ def min(a, axis=None):
 			if v < r:
 				r = v
 		return r
+
+def prod(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		p = 1.0
+		for i in nditer(a):
+			p *= float(i)
+		return p
+
+def ptp(a, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		mn = float(nditer(a)[0])
+		mx = float(nditer(a)[0])
+		p = 1.0
+		for i in nditer(a):
+			v = float(i)
+			if v > mx:
+				mx = v
+			if v < mn:
+				mn = v
+		return mx - mn
+
+def ravel(a):
+	out = empty((a.size,))
+	for i_index, i in enumerate(nditer(a)):
+		out[i_index,] = float(i)
+	return out
+
+def repeat(a, repeats, axis=None):
+	if axis:
+		raise (TypeError, "axis argument is not supported")
+	else:
+		out = empty((a.size * repeats,))
+		for i_index, i in enumerate(nditer(a)):
+			for r_index in xrange(repeats):
+				out[i_index * repeats + r_index,] = float(i)
+		return out
+
+def reshape(a, newshape):
+	out = a.copy()
+	out.shape = newshape
+	out.strides = _strides_for_shape(newshape)
+	out.size = _size_for_shape(newshape)
+	out.ndim = len(newshape)
+	return out
 
 def sum(a, axis=None):
 	if axis:
@@ -259,10 +450,23 @@ def sum(a, axis=None):
 
 newaxis = None
 
+def arange(size):
+	a = empty((size,))
+	a_iter = nditer(a)
+	for i in xrange(size):
+		a_iter[i] = i
+	return a
+
 def array(obj):
 	shape = _shape_from_object(obj)
 	a = ndarray(shape)
 	_assign_from_object(a, obj)
+	return a
+
+def eye(size):
+	a = zeros((size,size))
+	for i in xrange(size):
+		a[i,i] = 1.0
 	return a
 
 def zeros(shape):
