54bf768ce7148d399a4268476bf51281385d46e3
Wade Brainerd <wadeb@wadeb.com>
2017-04-21 10:01:55 -0700

    format setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'tinynumpy',
-  packages = ['tinynumpy'], # this must be the same as the name above
-  version = '1.2.0',
-  description = 'A lightweight, pure Python, numpy compliant ndarray class',
-  author = 'Wade Brainerd, Almar Klein',
-  author_email = 'wadetb@gmail.com',
-  url = 'https://github.com/wadetb/tinynumpy', # use the URL to the github repo
-  # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
-  keywords = ['Science', 'Research', 'Engineering', 'Software Development'],
-  classifiers = [],
-)
+    name='tinynumpy',
+    packages=['tinynumpy'],  # this must be the same as the name above
+    version='1.2.0',
+    description='A lightweight, pure Python, numpy compliant ndarray class',
+    author='Wade Brainerd, Almar Klein',
+    author_email='wadetb@gmail.com',
+    url='https://github.com/wadetb/tinynumpy',  # use the URL to the github repo
+    # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['Science', 'Research', 'Engineering', 'Software Development'],
+    classifiers=[],
+    )
